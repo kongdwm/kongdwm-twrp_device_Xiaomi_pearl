c6a4f3ef7f3dd6c84c2a92b597faf7fdea36c54e
kongdwm <3094744152@qq.com>
2024-08-05 21:48:55 +0800

    更新 twrp_pearl.mk

--- twrp_pearl.mk
+++ twrp_pearl.mk
@@ -6,7 +6,6 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
